c90593b67702e34ffdf0c0a48072f15b90d19eb7
alexsar kodehode <alexsar.kodehode@gmail.com>
2024-01-16 12:26:17 +0100

    1. Added three more images to my fish gallery and made a json file object for the images

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import './index.css'
-import IMAGES from './IMAGES/Images'
+import './app.css'
+import  ImageInfo  from "./IMAGES/Images.json"
 
-
-function app(){
+export default function App(){
+  console.log(ImageInfo)
 return (
   <>
-  <div>
-  <header>FishHub Gallery</header>
+  <div className='gallery'>
+  <header>Acvarium Gallery</header>
   </div>
-  <div>
-  <main>
-    <p>Welcome to Fishhub. Here we display the most beautiful fish around the world</p>
-    <img src={IMAGES.fish1} alt='./src/IMAGES/fish1.jpg'/>
-    <img src={IMAGES.fish2} alt='./src/IMAGES/fish2.jpg"'/>
-    <img src={IMAGES.fish3} alt='.src//IMAGES/fish3.jpg'/>
+  <div className='mainCard'>
+    <main>
+  <img src={ImageInfo[0].src} alt="" />
+    <p>{ImageInfo[0].text}</p>
+
+    <img src={ImageInfo[1].src} alt="" />
+    <p>{ImageInfo[1].text} test endring</p>
+
+    <img src={ImageInfo[2].src} alt="" />
+    <p>{ImageInfo[2].text} test 2</p>
+
+    <img src={ImageInfo[3].src} alt="" />
+    <p>{ImageInfo[3].text}</p>
+
+    <img src={ImageInfo[4].src} alt="" />
+    <p>{ImageInfo[4].text} test 3</p>
+
+    <img src={ImageInfo[5].src} alt="" />
+    <p>{ImageInfo[5].text}</p>
+ 
   </main>
   </div>
-  <footer>
+  
+  <footer className='footer'>
     <p>adresse: Grand park avenue 73</p>
     <p> email: fishHub@gmail.com</p>
-   
   </footer>
   </>
 )
 }
-
-export default app; 
